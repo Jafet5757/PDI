7440ad6168d52a99926eec5715c9a63479474e11
Jafet <moranorozcokevinjafet@gmail.com>
2024-05-13 20:03:12 -0600

    filter N

--- FiltroPrewit.py
+++ FiltroPrewit.py
@@ -14,31 +14,31 @@ class Filter:
     self.widthMask = len(mask[0])
     self.heightMask = len(mask)
 
-  def applyFilter(self):
+  def applyFilter(self, N = 0):
     for x in range(1, self.width-1):
       for y in range(1, self.height-1):
-        self.applyMask(x, y)
+        self.applyMask(x, y, N)
     return self.newImage
   
-  def applyMask(self, x, y):
+  def applyMask(self, x, y, N ):
     total = 0
     for i in range(self.widthMask):
       for j in range(self.heightMask):
         total += self.image.getpixel((x-1+i, y-1+j)) * self.mask[i][j]
-    self.newImage.putpixel((x, y), int(total/(self.widthMask*self.heightMask)))
+    self.newImage.putpixel((x, y), int(total/(self.widthMask*(self.heightMask+N))))
 
 def main():
   image = Image.open("tigre.jpeg").convert("L")
   plt.subplot(1, 2, 1)
   plt.imshow(image, cmap='gray')
   plt.title("Imagen original")
-  #mask = [[1, 0, -1], [2, 0, -2], [1, 0, -1]] # Sobel
+  mask = [[1, 0, -1], [2, 0, -2], [1, 0, -1]] # Sobel
   #mask = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]] # Prewit fila
   #mask = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]] # Prewit columna
   #mask = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]] # Laplaciano
   #mask = [[0, 0, 0], [0, 1, 0], [0, 0, 0]] # identidad
   #mask = [[1, 1, 1], [1, 1, 1], [1, 1, 1]] # promedio
-  mask = [[1, 2, 1], [-2, 4, -2], [1, 2, 1]] # promedio ponderado
+  #mask = [[1, 1, 1], [1, 8, 1], [1, 1, 1]] # promedio ponderado, N = 8
   filter = Filter(image, mask)
   newImage = filter.applyFilter()
   plt.subplot(1, 2, 2)
