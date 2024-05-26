7f54cbd38b8481f67b7b762c406584856c48719a
Jafet <moranorozcokevinjafet@gmail.com>
2024-05-26 17:18:00 -0600

    n

--- FiltroPrewit.py
+++ FiltroPrewit.py
@@ -20,27 +20,27 @@ class Filter:
         self.applyMask(x, y, N)
     return self.newImage
   
-  def applyMask(self, x, y, N ):
+  def applyMask(self, x, y, N=1 ):
     total = 0
     for i in range(self.widthMask):
       for j in range(self.heightMask):
         total += self.image.getpixel((x-1+i, y-1+j)) * self.mask[i][j]
-    self.newImage.putpixel((x, y), int(total/(self.widthMask*(self.heightMask+N))))
+    self.newImage.putpixel((x, y), int(total/((self.widthMask*self.heightMask)+N)))
 
 def main():
   image = Image.open("pasto.jpg").convert("L")
   plt.subplot(1, 2, 1)
   plt.imshow(image, cmap='gray')
   plt.title("Imagen original")
-  mask = [[1, 0, -1], [2, 0, -2], [1, 0, -1]] # Sobel
-  #mask = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]] # Prewit fila
+  #mask = [[1, 0, -1], [2, 0, -2], [1, 0, -1]] # Sobel
+  mask = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]] # Prewit fila
   #mask = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]] # Prewit columna
   #mask = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]] # Laplaciano
   #mask = [[0, 0, 0], [0, 1, 0], [0, 0, 0]] # identidad
   #mask = [[1, 1, 1], [1, 1, 1], [1, 1, 1]] # promedio
   #mask = [[1, 1, 1], [1, 8, 1], [1, 1, 1]] # promedio ponderado, N = 8
   filter = Filter(image, mask)
-  newImage = filter.applyFilter(N=-7)
+  newImage = filter.applyFilter()
   plt.subplot(1, 2, 2)
   plt.imshow(newImage, cmap='gray')
   plt.title("Imagen filtrada")
